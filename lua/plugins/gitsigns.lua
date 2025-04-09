96356eb3dbc6c800c07b4f11a2a3bbe4c5d3b74a
yogeshbhutkar <yogesh.bhutkar@rtcamp.com>
2025-04-09 17:35:25 +0530

    feat: add git support

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -1,8 +1,13 @@
 return {
-	"lewis6991/gitsigns.nvim",
-	config = function ()
-		require("gitsigns").setup()
-	
-		vim.keymap.set("n", "<leader>gp", ":Gitsigns preview_hunk<CR>", {})
-	end
+	{
+		"tpope/vim-fugitive",
+	},
+	{
+		"lewis6991/gitsigns.nvim",
+		config = function()
+			require("gitsigns").setup()
+
+			vim.keymap.set("n", "<leader>gp", ":Gitsigns preview_hunk<CR>", {})
+		end,
+	},
 }
