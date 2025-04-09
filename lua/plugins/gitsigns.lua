d6f2ac5dc0a31cd06d9a58151748ebd670b09ce2
yogeshbhutkar <yogesh.bhutkar@rtcamp.com>
2025-04-09 17:42:28 +0530

    feat: add line blame binding

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -8,6 +8,7 @@ return {
 			require("gitsigns").setup()
 
 			vim.keymap.set("n", "<leader>gp", ":Gitsigns preview_hunk<CR>", {})
+			vim.keymap.set("n", "<leader>gt", ":Gitsigns toggle_current_line_blame<CR>", {})
 		end,
 	},
 }
